734bcf4c5e12ba33d5783d6b284f6ca45d864afe
steve <chilians@yahoo.com>
2024-03-30 19:35:39 -0700

    Initial Commit

---
+++ src/app/publishers/publishers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publishers',
+  standalone: true,
+  imports: [],
+  templateUrl: './publishers.component.html',
+  styleUrl: './publishers.component.scss'
+})
+export class PublishersComponent {
+
+}
